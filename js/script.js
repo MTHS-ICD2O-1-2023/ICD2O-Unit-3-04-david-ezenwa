32e779842d73d4dbad3a2e6888fa551352a97c41
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-06-18 16:59:25 +0000

    Fahrenheit to Celsius with JavaScript

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume.
+ */
+function fahrenheitToCelsius() {
+  // input
+  const temp = parseFloat(document.getElementById('temp').value)
+
+
+  // process
+  const tempInCelcius = 5 / 9 * (temp - 32)
+
+  // output
+  document.getElementById("temperature").innerHTML = 'The temperature in celcius is: ' + tempInCelcius.toFixed(3) + ' °C'
+}
